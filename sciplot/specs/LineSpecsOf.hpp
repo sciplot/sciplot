0ae55c99cb7529f0790120175a70825d582ece6b
Allan Leal <allanmulin@gmail.com>
2021-01-29 09:47:03 +0100

    Implemented tests for LineSpecsOf

--- sciplot/specs/LineSpecsOf.hpp
+++ sciplot/specs/LineSpecsOf.hpp
@@ -27,7 +27,6 @@
 
 // sciplot includes
 #include <sciplot/default.hpp>
-#include <sciplot/enums.hpp>
 #include <sciplot/specs/Specs.hpp>
 #include <sciplot/util.hpp>
 
@@ -42,75 +41,39 @@ class LineSpecsOf : virtual public internal::Specs<DerivedSpecs>
     /// Construct a default LineSpecsOf instance.
     LineSpecsOf();
 
+    /// Set the line style of the underlying line object.
+    auto lineStyle(int value) -> DerivedSpecs&;
+
+    /// Set the line type of the underlying line object.
+    auto lineType(int value) -> DerivedSpecs&;
+
+    /// Set the line width of the underlying line object.
+    auto lineWidth(int value) -> DerivedSpecs&;
+
+    /// Set the line color of the underlying line object.
+    auto lineColor(std::string value) -> DerivedSpecs&;
+
+    /// Set the dash type of the underlying line object.
+    auto dashType(int value) -> DerivedSpecs&;
+
     /// Convert this LineSpecsOf object into a gnuplot formatted string.
     auto repr() const -> std::string;
 
-    /// Set the line style of the plot.
-    auto lineStyle(std::size_t value) -> DerivedSpecs&
-    {
-        m_linestyle = internal::str(value);
-        return static_cast<DerivedSpecs&>(*this);
-    }
-
-    /// Set the line type of the plot.
-    auto lineType(std::size_t value) -> DerivedSpecs&
-    {
-        m_linetype = internal::str(value);
-        return static_cast<DerivedSpecs&>(*this);
-    }
-
-    /// Set the line width of the plot.
-    auto lineWidth(std::size_t value) -> DerivedSpecs&
-    {
-        m_linewidth = internal::str(value);
-        return static_cast<DerivedSpecs&>(*this);
-    }
-
-    /// Set the line color of the plot.
-    auto lineColor(std::string value) -> DerivedSpecs&
-    {
-        m_linecolor = "'" + value + "'";
-        return static_cast<DerivedSpecs&>(*this);
-    }
-
-    /// Set the dash type of the plot.
-    auto dashType(std::size_t value) -> DerivedSpecs&
-    {
-        m_dashtype = internal::str(value);
-        return static_cast<DerivedSpecs&>(*this);
-    }
-
-    /// Set the dash type of the plot.
-    /// The density parameter only works for the "solid" fill mode and must be in [0,1].
-    auto fillStyle(fillstyle value, float density = 1.0F) -> DerivedSpecs&  // TODO: This fillStyle method should not belong to LineSpecsOf.
-    {
-        m_fillstyle = gnuplot::fillstylestr(value);
-        if (value == fillstyle::solid)
-        {
-            density = density < 0.0F ? 0.0F : (density > 1.0F ? 1.0F : density);
-            m_fillstyle += " " + internal::str(density);
-        }
-        return static_cast<DerivedSpecs&>(*this);
-    }
-
   private:
-    /// The line style of the plot (e.g., "ls 2").
+    /// The line style of the underlying line object (e.g., "ls 2").
     std::string m_linestyle;
 
-    /// The line type of the plot (e.g., "lt 3").
+    /// The line type of the underlying line object (e.g., "lt 3").
     std::string m_linetype;
 
-    /// The line width of the plot (e.g., "lw 2").
+    /// The line width of the underlying line object (e.g., "lw 2").
     std::string m_linewidth;
 
-    /// The line color of the plot (e.g., "lc rgb '#FF00FF'").
+    /// The line color of the underlying line object (e.g., "lc rgb '#FF00FF'").
     std::string m_linecolor;
 
-    /// The dash type of the plot (e.g., "dt 2").
+    /// The dash type of the underlying line object (e.g., "dt 2").
     std::string m_dashtype;
-
-    /// The fill style of the plot (e.g., "fs solid 0.5").
-    std::string m_fillstyle;
 };
 
 template <typename DerivedSpecs>
@@ -120,16 +83,50 @@ LineSpecsOf<DerivedSpecs>::LineSpecsOf()
 }
 
 template <typename DerivedSpecs>
+auto LineSpecsOf<DerivedSpecs>::lineStyle(int value) -> DerivedSpecs&
+{
+    m_linestyle = internal::str(value);
+    return static_cast<DerivedSpecs&>(*this);
+}
+
+template <typename DerivedSpecs>
+auto LineSpecsOf<DerivedSpecs>::lineType(int value) -> DerivedSpecs&
+{
+    m_linetype = internal::str(value);
+    return static_cast<DerivedSpecs&>(*this);
+}
+
+template <typename DerivedSpecs>
+auto LineSpecsOf<DerivedSpecs>::lineWidth(int value) -> DerivedSpecs&
+{
+    m_linewidth = internal::str(value);
+    return static_cast<DerivedSpecs&>(*this);
+}
+
+template <typename DerivedSpecs>
+auto LineSpecsOf<DerivedSpecs>::lineColor(std::string value) -> DerivedSpecs&
+{
+    m_linecolor = "'" + value + "'";
+    return static_cast<DerivedSpecs&>(*this);
+}
+
+template <typename DerivedSpecs>
+auto LineSpecsOf<DerivedSpecs>::dashType(int value) -> DerivedSpecs&
+{
+    m_dashtype = internal::str(value);
+    return static_cast<DerivedSpecs&>(*this);
+}
+
+template <typename DerivedSpecs>
 auto LineSpecsOf<DerivedSpecs>::repr() const -> std::string
 {
     std::stringstream ss;
-    ss << gnuplot::optionvaluestr("linestyle", m_linestyle);
-    ss << gnuplot::optionvaluestr("linetype", m_linetype);
-    ss << gnuplot::optionvaluestr("linewidth", m_linewidth);
-    ss << gnuplot::optionvaluestr("linecolor", m_linecolor);
-    ss << gnuplot::optionvaluestr("dashtype", m_dashtype);
-    ss << gnuplot::optionvaluestr("fillstyle", m_fillstyle);
-    return ss.str();
+    ss << gnuplot::optionValueStr("linestyle", m_linestyle);
+    ss << gnuplot::optionValueStr("linetype", m_linetype);
+    ss << gnuplot::optionValueStr("linewidth", m_linewidth);
+    ss << gnuplot::optionValueStr("linecolor", m_linecolor);
+    ss << gnuplot::optionValueStr("dashtype", m_dashtype);
+    return internal::removeExtraWhitespaces(ss.str());
 }
 
 } // namespace sciplot
