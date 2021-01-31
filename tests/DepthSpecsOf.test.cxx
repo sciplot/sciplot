76202eb388b5313640be1ba938bfe6fbc0f069d5
Allan Leal <allanmulin@gmail.com>
2021-01-31 18:52:40 +0100

    Implemented tests for DepthSpecsOf

---
+++ tests/DepthSpecsOf.test.cxx
@@ -0,0 +1,50 @@
+// sciplot - a modern C++ scientific plotting library powered by gnuplot
+// https://github.com/sciplot/sciplot
+//
+// Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+//
+// Copyright (c) 2018-2021 Allan Leal
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Catch includes
+#include <tests/catch.hpp>
+
+// sciplot includes
+#include <sciplot/specs/DepthSpecsOf.hpp>
+using namespace sciplot;
+
+TEST_CASE("DepthSpecs", "[specs]")
+{
+    auto default_depth = DepthSpecs();
+    default_depth.back();
+
+    auto depth = DepthSpecs();
+
+    CHECK( depth.repr() == default_depth.repr() );
+
+    depth.front();
+    CHECK( depth.repr() == "front" );
+
+    depth.back();
+    CHECK( depth.repr() == "back" );
+
+    depth.behind();
+    CHECK( depth.repr() == "behind" );
+}
