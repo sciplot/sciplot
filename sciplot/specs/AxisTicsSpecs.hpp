8512c76a5a69ba1e26e8f81cfa543aa181ff61ce
Allan Leal <allanmulin@gmail.com>
2021-01-31 18:24:29 +0100

    AxisTicsSpecs now inherits from TicsSpecs

--- sciplot/specs/AxisTicsSpecs.hpp
+++ sciplot/specs/AxisTicsSpecs.hpp
@@ -29,59 +29,17 @@
 #include <vector>
 
 // sciplot includes
-#include <sciplot/default.hpp>
-#include <sciplot/specs/OffsetSpecsOf.hpp>
-#include <sciplot/specs/ShowSpecsOf.hpp>
-#include <sciplot/specs/TextSpecsOf.hpp>
+#include <sciplot/specs/TicsSpecs.hpp>
 #include <sciplot/util.hpp>
 
 namespace sciplot {
 
-/// The class used to specify options for tics.
-class AxisTicsSpecs : public TextSpecsOf<AxisTicsSpecs>, public OffsetSpecsOf<AxisTicsSpecs>, public ShowSpecsOf<AxisTicsSpecs>
+/// The class used to specify options for tics of a specific axis.
+class AxisTicsSpecs : public TicsSpecs
 {
   public:
     /// Construct a default AxisTicsSpecs instance.
     AxisTicsSpecs(std::string axis);
-
-    /// Set the tics to be displayed along the axis.
-    auto alongAxis() -> AxisTicsSpecs&;
-
-    /// Set the tics to be displayed along the border.
-    auto alongBorder() -> AxisTicsSpecs&;
-
-    /// Set the tics to be mirrored on the opposite border if `true`.
-    auto mirror(bool value=true) -> AxisTicsSpecs&;
-
-    /// Set the tics to be displayed inside the graph.
-    auto insideGraph() -> AxisTicsSpecs&;
-
-    /// Set the tics to be displayed outside the graph.
-    auto outsideGraph() -> AxisTicsSpecs&;
-
-    /// Set the tics to be rotated by 90 degrees if `true`.
-    auto rotate(bool value=true) -> AxisTicsSpecs&;
-
-    /// Set the tics to be rotated by given degrees.
-    auto rotateBy(double degrees) -> AxisTicsSpecs&;
-
-    /// Set the tics to be displayed on the front of all plot elements.
-    auto stackFront() -> AxisTicsSpecs&;
-
-    /// Set the tics to be displayed on the back of all plot elements.
-    auto stackBack() -> AxisTicsSpecs&;
-
-    /// Set the scale for the major tics (identical to method scaleMajorBy).
-    auto scaleBy(double value) -> AxisTicsSpecs&;
-
-    /// Set the scale for the major tics.
-    auto scaleMajorBy(double value) -> AxisTicsSpecs&;
-
-    /// Set the scale for the minor tics.
-    auto scaleMinorBy(double value) -> AxisTicsSpecs&;
-
-    /// Set the format of the tics using a format expression (`"%.2f"`)
-    auto format(std::string fmt) -> AxisTicsSpecs&;
 
     /// Set the values of the tics to be identified automatically or not.
     auto automatic() -> AxisTicsSpecs&;
@@ -117,24 +75,6 @@ class AxisTicsSpecs : public TextSpecsOf<AxisTicsSpecs>, public OffsetSpecsOf<Ax
     /// The name of the axis associated with these tics.
     std::string m_axis;
 
-    /// The option indicating if tics are displayed along axis or border.
-    std::string m_along;
-
-    /// The mirror option of the tics.
-    std::string m_mirror;
-
-    /// The rotate option for the tics.
-    std::string m_rotate;
-
-    /// The depth where the tics are displayed (back or front).
-    std::string m_depth;
-
-    /// The place where the tics are displayed (in or out).
-    std::string m_inout;
-
-    /// The format expression for formatting the display of the tics.
-    std::string m_format;
-
     /// The start value for the tics.
     std::string m_start;
 
@@ -149,107 +89,17 @@ class AxisTicsSpecs : public TextSpecsOf<AxisTicsSpecs>, public OffsetSpecsOf<Ax
 
     /// The extra values/labels of the tics to be displayed.
     std::string m_add;
-
-    /// The scale of the major tics.
-    double m_scalemajor = 1.0;
-
-    /// The scale of the minor tics.
-    double m_scaleminor = 1.0;
 };
 
-AxisTicsSpecs::AxisTicsSpecs(std::string axis)
-: m_axis(axis)
+inline AxisTicsSpecs::AxisTicsSpecs(std::string axis)
+: TicsSpecs(), m_axis(axis)
 {
     if(axis.empty())
         throw std::runtime_error("You have provided an empty string "
             "in `axis` argument of constructor AxisTicsSpecs(axis).");
-    alongBorder();
-    mirror(internal::DEFAULT_TICS_MIRROR);
-    outsideGraph();
-    rotate(internal::DEFAULT_TICS_ROTATE);
-    stackFront();
-    scaleMajorBy(internal::DEFAULT_TICS_SCALE_MAJOR_BY);
-    scaleMinorBy(internal::DEFAULT_TICS_SCALE_MINOR_BY);
 }
 
-auto AxisTicsSpecs::alongAxis() -> AxisTicsSpecs&
-{
-    m_along = "axis";
-    return *this;
-}
-
-auto AxisTicsSpecs::alongBorder() -> AxisTicsSpecs&
-{
-    m_along = "border";
-    return *this;
-}
-
-auto AxisTicsSpecs::mirror(bool value) -> AxisTicsSpecs&
-{
-    m_mirror = value ? "mirror" : "nomirror";
-    return *this;
-}
-
-auto AxisTicsSpecs::rotate(bool value) -> AxisTicsSpecs&
-{
-    m_rotate = value ? "rotate" : "norotate";
-    return *this;
-}
-
-auto AxisTicsSpecs::rotateBy(double degrees) -> AxisTicsSpecs&
-{
-    m_rotate = "rotate by " + internal::str(degrees);
-    return *this;
-}
-
-auto AxisTicsSpecs::stackFront() -> AxisTicsSpecs&
-{
-    m_depth = "front";
-    return *this;
-}
-
-auto AxisTicsSpecs::stackBack() -> AxisTicsSpecs&
-{
-    m_depth = "back";
-    return *this;
-}
-
-auto AxisTicsSpecs::insideGraph() -> AxisTicsSpecs&
-{
-    m_inout = "in";
-    return *this;
-}
-
-auto AxisTicsSpecs::outsideGraph() -> AxisTicsSpecs&
-{
-    m_inout = "out";
-    return *this;
-}
-
-auto AxisTicsSpecs::scaleBy(double value) -> AxisTicsSpecs&
-{
-    return scaleMajorBy(value);
-}
-
-auto AxisTicsSpecs::scaleMajorBy(double value) -> AxisTicsSpecs&
-{
-    m_scalemajor = value;
-    return *this;
-}
-
-auto AxisTicsSpecs::scaleMinorBy(double value) -> AxisTicsSpecs&
-{
-    m_scaleminor = value;
-    return *this;
-}
-
-auto AxisTicsSpecs::format(std::string fmt) -> AxisTicsSpecs&
-{
-    m_format = "'" + fmt + "'";
-    return *this;
-}
-
-auto AxisTicsSpecs::automatic() -> AxisTicsSpecs&
+inline auto AxisTicsSpecs::automatic() -> AxisTicsSpecs&
 {
     // clear strings related to tics values/labels
     m_start = "";
@@ -259,28 +109,28 @@ auto AxisTicsSpecs::automatic() -> AxisTicsSpecs&
     return *this;
 }
 
-auto AxisTicsSpecs::start(double value) -> AxisTicsSpecs&
+inline auto AxisTicsSpecs::start(double value) -> AxisTicsSpecs&
 {
     m_start = internal::str(value) + ", ";
     m_at = m_start + m_increment + m_end;
     return *this;
 }
 
-auto AxisTicsSpecs::increment(double value) -> AxisTicsSpecs&
+inline auto AxisTicsSpecs::increment(double value) -> AxisTicsSpecs&
 {
     m_increment = internal::str(value);
     m_at = m_start + m_increment + m_end;
     return *this;
 }
 
-auto AxisTicsSpecs::end(double value) -> AxisTicsSpecs&
+inline auto AxisTicsSpecs::end(double value) -> AxisTicsSpecs&
 {
     m_end = ", " + internal::str(value);
     m_at = m_start + m_increment + m_end;
     return *this;
 }
 
-auto AxisTicsSpecs::interval(double start, double increment, double end) -> AxisTicsSpecs&
+inline auto AxisTicsSpecs::interval(double start, double increment, double end) -> AxisTicsSpecs&
 {
     if(increment <= 0.0) throw std::runtime_error("The `increment` argument in method AxisTicsSpecs::interval must be positive.");
     if(end <= start) throw std::runtime_error("The `end` argument in method AxisTicsSpecs::interval must be greater than `start`.");
@@ -290,7 +140,7 @@ auto AxisTicsSpecs::interval(double start, double increment, double end) -> Axis
     return *this;
 }
 
-auto AxisTicsSpecs::at(const std::vector<double>& values) -> AxisTicsSpecs&
+inline auto AxisTicsSpecs::at(const std::vector<double>& values) -> AxisTicsSpecs&
 {
     std::stringstream ss;
     ss << "(";
@@ -301,7 +151,7 @@ auto AxisTicsSpecs::at(const std::vector<double>& values) -> AxisTicsSpecs&
     return *this;
 }
 
-auto AxisTicsSpecs::at(const std::vector<double>& values, const std::vector<std::string>& labels) -> AxisTicsSpecs&
+inline auto AxisTicsSpecs::at(const std::vector<double>& values, const std::vector<std::string>& labels) -> AxisTicsSpecs&
 {
     std::stringstream ss;
     ss << "(";
@@ -312,7 +162,7 @@ auto AxisTicsSpecs::at(const std::vector<double>& values, const std::vector<std:
     return *this;
 }
 
-auto AxisTicsSpecs::add(const std::vector<double>& values) -> AxisTicsSpecs&
+inline auto AxisTicsSpecs::add(const std::vector<double>& values) -> AxisTicsSpecs&
 {
     std::stringstream ss;
     ss << "add (";
@@ -323,7 +173,7 @@ auto AxisTicsSpecs::add(const std::vector<double>& values) -> AxisTicsSpecs&
     return *this;
 }
 
-auto AxisTicsSpecs::add(const std::vector<double>& values, const std::vector<std::string>& labels) -> AxisTicsSpecs&
+inline auto AxisTicsSpecs::add(const std::vector<double>& values, const std::vector<std::string>& labels) -> AxisTicsSpecs&
 {
     std::stringstream ss;
     ss << "add (";
@@ -334,9 +184,9 @@ auto AxisTicsSpecs::add(const std::vector<double>& values, const std::vector<std
     return *this;
 }
 
-auto AxisTicsSpecs::repr() const -> std::string
+inline auto AxisTicsSpecs::repr() const -> std::string
 {
-    const auto show = ShowSpecsOf<AxisTicsSpecs>::repr();
+    const auto show = ShowSpecsOf<TicsSpecs>::repr();
     if(show == "no")
         return "unset " + m_axis + "tics";
 
@@ -348,16 +198,7 @@ auto AxisTicsSpecs::repr() const -> std::string
         throw std::runtime_error("You have called method AxisTicsSpecs::end but not AxisTicsSpecs::start.");
 
     std::stringstream ss;
-    ss << "set " << m_axis << "tics" << " ";
-    ss << m_along << " ";
-    ss << m_mirror << " ";
-    ss << m_depth << " ";
-    ss << m_inout << " ";
-    ss << "scale " << m_scalemajor << "," << m_scaleminor << " ";
-    ss << m_rotate << " ";
-    ss << OffsetSpecsOf<AxisTicsSpecs>::repr() << " ";
-    ss << TextSpecsOf<AxisTicsSpecs>::repr() << " ";
-    ss << m_format << " ";
+    ss << TicsSpecs::repr(m_axis) << " ";
     ss << m_at << " ";
     ss << m_add << " ";
     return internal::removeExtraWhitespaces(ss.str());
