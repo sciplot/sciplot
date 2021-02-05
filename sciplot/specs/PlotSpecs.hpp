581e3598a5bb49550cabc3af2c5712a82a96c87e
Allan Leal <allanmulin@gmail.com>
2021-02-05 14:24:32 +0100

    PlotSpecs now support xtic and ytic commands

--- sciplot/specs/PlotSpecs.hpp
+++ sciplot/specs/PlotSpecs.hpp
@@ -25,13 +25,7 @@
 
 #pragma once
 
-// C++ includes
-#include <algorithm>
-#include <limits>
-#include <numeric>
-
 // sciplot includes
-#include <sciplot/enums.hpp>
 #include <sciplot/specs/FillSpecsOf.hpp>
 #include <sciplot/specs/LineSpecsOf.hpp>
 #include <sciplot/specs/PointSpecsOf.hpp>
@@ -39,74 +33,103 @@
 
 namespace sciplot {
 
-/// The class where options for the plot function are specified.
+/// The class where options for the plotted element can be specified.
 class PlotSpecs : public LineSpecsOf<PlotSpecs>, public PointSpecsOf<PlotSpecs>, public FillSpecsOf<PlotSpecs>
 {
   public:
-    /// Undefine / ignore column usage value. See use().
-    static constexpr int USE_AUTO = std::numeric_limits<int>::min();
-
     /// Construct a PlotSpecs instance.
-    /// @param what A string representing what to be plot (e.g., "'filename' u 1:2", "sin(x)", etc.)
-    PlotSpecs(std::string what, std::string with);
+    /// @param what The string representing `what` to be plot (e.g., "'filename'", "sin(x)")
+    /// @param use The string representing the `using` expression (e.g., "using 1:2", "using 4:6:8:9")
+    /// @param with The string representing the `with plotstyle` expression (e.g., "lines", "linespoints", "dots")
+    PlotSpecs(std::string what, std::string use, std::string with);
 
     /// Convert this PlotSpecs object into a gnuplot formatted string.
     auto repr() const -> std::string;
 
-    /// Set the label to be displayed for the plot object.
-    auto title(std::string value) -> PlotSpecs&
+    /// Set the legend label of the plotted element.
+    auto label(std::string text) -> PlotSpecs&
     {
-        m_title = gnuplot::titlestr(value);
+        m_title = "'" + text + "'";
         return *this;
     }
 
-    /// Set which columns from the data file to use for plot data or tick labels. Resembles the "using" directive for a plot.
-    /// Pass an USE_AUTO in any of these values to "undefine" that value, e.g. to use column 2 for y, do: plot.use(USE_AUTO, 2);
-    /// To use strings as tick labels, you can pass them in the corresponding data column in the plot.draw() call.
-    auto use(int xcol = USE_AUTO, int ycol = USE_AUTO, int zcol = USE_AUTO, int xtic = USE_AUTO, int x2tic = USE_AUTO, int ytic = USE_AUTO, int y2tic = USE_AUTO, int ztic = USE_AUTO) -> PlotSpecs&
+    /// Set the legend label of the plotted element to be retrieved from the header of column.
+    auto labelFromColumnHeader() -> PlotSpecs&
     {
-        std::vector<std::pair<unsigned int, std::string>> values = {
-            {xcol, std::to_string(xcol)},
-            {ycol, std::to_string(ycol)},
-            {zcol, std::to_string(zcol)},
-            {xtic, "xtic(" + std::to_string(xtic) + ")"},
-            {x2tic, "x2tic(" + std::to_string(x2tic) + ")"},
-            {ytic, "ytic(" + std::to_string(ytic) + ")"},
-            {y2tic, "y2tic(" + std::to_string(y2tic) + ")"},
-            {ztic, "ztic(" + std::to_string(ztic) + ")"}};
-        std::vector<std::string> strings;
-        // filter out all valuzes we don't want to use and put the remaining one int strings
-        std::for_each(values.begin(), values.end(), [&strings](const std::pair<unsigned int, std::string>& v) { if (v.first != USE_AUTO) { strings.push_back(v.second); } });
-        // join all remaining values using ":"
-        m_using = std::accumulate(std::next(strings.begin()), strings.end(), strings[0], [](const std::string& a, const std::string& b) { return a + ":" + b; });
+        m_title = "columnheader";
+        return *this;
+    }
+
+    /// Set the legend label of the plotted element to be retrieved from the header of a column with given index.
+    auto labelFromColumnHeader(int icolumn) -> PlotSpecs&
+    {
+        m_title = "columnheader(" + std::to_string(icolumn) + ")";
+        return *this;
+    }
+
+    /// Set the legend label of the plotted element to be ignored.
+    auto labelNone() -> PlotSpecs&
+    {
+        m_title = "notitle";
+        return *this;
+    }
+
+    /// Set the legend label to be determined automatically from the plot expression.
+    auto labelDefault() -> PlotSpecs&
+    {
+        m_title = "";
+        return *this;
+    }
+
+    /// Set the column in the data file containing the tic labels for *x* axis.
+    auto xtics(ColumnIndex icol) -> PlotSpecs&
+    {
+        m_xtic = "xtic(stringcolumn(" + icol.value + "))"; // xtic(stringcolumn(1)) or xtic(stringcolumn('Name'))
+        return *this;
+    }
+
+    /// Set the column in the data file containing the tic labels for *y* axis.
+    auto ytics(ColumnIndex icol) -> PlotSpecs&
+    {
+        m_ytic = "ytic(stringcolumn(" + icol.value + "))"; // ytic(stringcolumn(1)) or ytic(stringcolumn('Name'))
         return *this;
     }
 
   private:
-    /// The what to be plotted as a gnuplot formatted string (e.g., "sin(x)").
+    /// The string representing `what` to be plot (e.g., "'filename'", "sin(x)").
     std::string m_what;
 
-    /// The style of the plot (lines, points, linespoints) as a gnuplot formatted string (e.g., "with linespoints").
+    /// The string representing the `using` expression (e.g., "using 1:2", "using 4:6:8:9").
+    std::string m_using;
+
+    /// The string representing the `with plotstyle` expression (e.g., "lines", "linespoints", "dots").
     std::string m_with;
 
     /// The title of the plot as a gnuplot formatted string (e.g., "title 'sin(x)'").
     std::string m_title;
 
-    /// Select which columns from the data file to use for plot data or tick labels (e.g. "using 1:xtic(2)").
-    std::string m_using;
+    /// The column in the data file containing the x tic labels.
+    std::string m_xtic;
+
+    /// The column in the data file containing the y tic labels.
+    std::string m_ytic;
 };
 
-PlotSpecs::PlotSpecs(std::string what, std::string with)
-: m_what(what), m_with(with)
+PlotSpecs::PlotSpecs(std::string what, std::string use, std::string with)
+: m_what(what), m_using(use), m_with(with)
 {
     lineWidth(internal::DEFAULT_LINEWIDTH);
 }
 
 auto PlotSpecs::repr() const -> std::string
 {
+    std::string use = m_using;
+    use += ":" + m_xtic + ":" + m_ytic;
+    use = internal::trimright(use, ':'); // remove any leading `:` in string `use`
+
     std::stringstream ss;
     ss << m_what << " ";
-    ss << gnuplot::optionValueStr("using", m_using);
+    ss << gnuplot::optionValueStr("using", use);
     ss << gnuplot::optionValueStr("title", m_title);
     ss << gnuplot::optionValueStr("with", m_with);
     ss << LineSpecsOf<PlotSpecs>::repr() << " ";
