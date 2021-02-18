be2ee41b530f9cbcaefd343dad049b7d7b65f524
Allan Leal <allanmulin@gmail.com>
2021-02-19 00:43:25 +0100

    Implemented range and Strings

--- sciplot/Vec.hpp
+++ sciplot/Vec.hpp
@@ -28,20 +28,36 @@
 // C++ includes
 #include <algorithm>
 #include <functional>
+#include <string>
 #include <valarray>
+#include <vector>
 
 namespace sciplot {
 
 /// A convenient type alias for std::valarray<double>
 using Vec = std::valarray<double>;
 
-/// Return an array with uniform incrementes from a given initial value to a final one
+/// A convenient type alias for std::vector<std::string>
+using Strings = std::vector<std::string>;
+
+/// Return an array with uniform increments from a given initial value to a final one
 template <typename T0, typename T1, typename U = double>
 auto linspace(T0 x0, T1 x1, std::size_t numintervals) -> std::valarray<U>
 {
     std::valarray<U> result(numintervals + 1);
-    for (std::size_t i = 0; i <= numintervals; ++i)
+    for(std::size_t i = 0; i <= numintervals; ++i)
         result[i] = x0 + i * (x1 - x0) / static_cast<U>(numintervals);
+    return result;
+}
+
+/// Return an array with unit increment from a given initial value to a final one
+template <typename U = double>
+auto range(int x0, int x1) -> Vec
+{
+    const auto incr = (x1 > x0) ? +1 : -1;
+    std::valarray<U> result(x1 - x0 + 1);
+    for(std::size_t i = 0; i < result.size(); ++i)
+        result[i] = x0 + i * incr;
     return result;
 }
 
