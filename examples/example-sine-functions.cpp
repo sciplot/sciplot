4eb924ccaa53f9ee49b84ebf718e832e46de43b0
Allan Leal <allanmulin@gmail.com>
2021-02-24 08:50:37 +0100

    Improve example-sine-functions (which also fix i * valarray issue in msvc)

--- examples/example-sine-functions.cpp
+++ examples/example-sine-functions.cpp
@@ -41,11 +41,21 @@ int main(int argc, char** argv)
 
     // Set the x and y ranges
     plot.xrange(0.0, PI);
-    plot.yrange(0.0, 6.0);
+    plot.yrange(0.0, 1.0);
 
-    // Plot i*sin(x) from i = 1 to i = 6
-    for (auto i = 1; i <= 6; ++i)
-        plot.drawCurve(x, i * std::sin(x)).label(std::to_string(i) + "⋅sin(x)");
+    // Set the legend to be on the bottom along the horizontal
+    plot.legend()
+        .atOutsideBottom()
+        .displayHorizontal()
+        .displayExpandWidthBy(2);
+
+    // Plot sin(i*x) from i = 1 to i = 6
+    plot.drawCurve(x, std::sin(1.0 * x)).label("sin(x)");
+    plot.drawCurve(x, std::sin(2.0 * x)).label("sin(2x)");
+    plot.drawCurve(x, std::sin(3.0 * x)).label("sin(3x)");
+    plot.drawCurve(x, std::sin(4.0 * x)).label("sin(4x)");
+    plot.drawCurve(x, std::sin(5.0 * x)).label("sin(5x)");
+    plot.drawCurve(x, std::sin(6.0 * x)).label("sin(6x)");
 
     // Show the plot in a pop-up window
     plot.show();
