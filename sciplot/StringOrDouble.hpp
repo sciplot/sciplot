9830097c81cd8eded2d82ee052de4d8cfc533cbd
Thomas Schlegel <thomas.schlegel@digos.eu>
2021-03-01 21:41:06 +0100

    Completed comment

--- sciplot/StringOrDouble.hpp
+++ sciplot/StringOrDouble.hpp
@@ -30,7 +30,7 @@
 
 namespace sciplot {
 
-/// An auxiliary type used to
+/// An auxiliary type used to store a string value, while the input can also be a double.
 struct StringOrDouble
 {
     /// Construct a default StringOrDouble object.
