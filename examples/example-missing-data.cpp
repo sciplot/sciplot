562bcaaf39e2940798a1a2c6cc47ad47167e8154
Thomas Schlegel <thomas.schlegel@digos.eu>
2021-03-10 08:47:54 +0100

    Added example for missing data

---
+++ examples/example-missing-data.cpp
@@ -0,0 +1,49 @@
+// sciplot - a modern C++ scientific plotting library powered by gnuplot
+// https://github.com/sciplot/sciplot
+//
+// Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+//
+// Copyright (c) 2018-2021 Allan Leal
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// sciplot includes
+#include <sciplot/sciplot.hpp>
+using namespace sciplot;
+
+int main(int argc, char** argv)
+{
+    // Create vectors with some missing y values
+    Vec x = {0, 1, 2, 3, 4, 5, 6, 7};
+    Vec y = {3.2, 7.3, NaN, 2.8, 8.9, NaN, 5.0, 1.8};
+
+    // Create a Plot object
+    Plot plot;
+
+    // This disables the deletion of the created gnuplot script and data file.
+    plot.autoclean(false);
+
+    // Plot the data with linespoints
+    plot.drawBroken("linespoints", x, y).label("Data");
+
+    plot.show();
+
+    // Save the plot to a PDF file
+    plot.save("example-missing-data.pdf");
+}
